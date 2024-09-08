5f662b43fe22338819244e2e3020243576a0ebfd
abdur28 <abdurrahmanidris28@gmail.com>
2024-09-08 14:02:40 +0300

    initial commit

--- app.ts
+++ app.ts
@@ -10,7 +10,7 @@ const app = express();
 // Database connection
 const connectDB = async () => {
     try {
-      await mongoose.connect(process.env.MONGODB_URI as string);
+      await mongoose.connect(process.env.MONGODB_URI);
       console.log(`MongoDB Connected: ${mongoose.connection.host}`);
     } catch (error) {
       console.error(error);
