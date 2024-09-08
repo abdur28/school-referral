ae291d3fef4961aca8b31eccf9e150ffab9b8bc3
abdur28 <abdurrahmanidris28@gmail.com>
2024-09-08 13:53:09 +0300

    initial commit

--- app.ts
+++ app.ts
@@ -1,5 +1,5 @@
 const express = require('express');
-const db  = require('./config/db');
+const connectDB  = require('./config/db');
 const referralRoutes = require('./src/routes/referralRoutes');
 const cookieParser = require('cookie-parser');
 const path = require('path');
@@ -7,7 +7,7 @@ const path = require('path');
 const app = express();
 
 // Database connection
-db.connectDB();
+connectDB();
 
 // Body parsers
 app.use(express.json());
